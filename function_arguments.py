512585eea5bd8b637d5796f89c814c02cc69cbf8
linkwithlionhart <linkwithlionhart@gmail.com>
2024-10-23 20:21:04 -0700

    evening practice'

--- function_arguments.py
+++ function_arguments.py
@@ -44,9 +44,13 @@ print(assign_table(name='Martha', table_number=3, vip_status=True))
 print(assign_table(4, 'Karla'))
 
 #2 Variable number of arguments: *args (positional arugment packing)
-"""Notice how the print() function does not care how many arguments we pass to it. It has no expectation that we are going to pass in one argument or even a million! So the question is, how does print() accomplish this?
+"""
+Notice how the print() function does not care how many arguments we pass to it. 
+It has no expectation that we are going to pass in one argument or even a million! 
+So the question is, how does print() accomplish this?
 
-Well, in Python, there is an additional operator called the unpacking operator (*). The unpacking operator allows us to give our functions a variable number of arguments by performing what’s known as positional argument packing.
+Well, in Python, there is an additional operator called the unpacking operator (*). 
+The unpacking operator allows us to give our functions a variable number of arguments by performing what’s known as positional argument packing.
 """
 
 # Write your code below:
@@ -60,4 +64,74 @@ print_order(
   'Pancakes',
 )
 
+#3 Working with *args
+tables = {
+  1: {
+    'name': 'Jiho',
+    'vip_status': False,
+    'order': 'Orange Juice, Apple Juice'
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+print(tables)
 
+def assign_table(table_number, name, vip_status=False): 
+  tables[table_number]['name'] = name
+  tables[table_number]['vip_status'] = vip_status
+  tables[table_number]['order'] = ''
+
+# Write your code below:
+def assign_and_print_order(table_number, *order_items):
+  tables[table_number]['order'] = order_items
+  for item in order_items:
+    print(item)
+
+assign_table(2, 'Arwa', True)
+assign_and_print_order(2, 'Steak', 'Seabass', 'Wine Bottle')
+print(tables)
+
+#4 Variable number of keyword arguments: **kwargs
+"""
+Python doesn’t stop at allowing us to accept unlimited positional arguments; 
+it also gives us the power to define functions with unlimited keyword arguments. 
+The syntax is very similar but uses two asterisks ** instead of one. 
+We typically call these kwargs as a shorthand for keyword arguments.
+
+**kwargs takes the form of a dictionary with all the keyword argument values passed to arbitrary_keyword_args. 
+Since **kwargs is a dictionary, we can use standard dictionary functions like .get() to retrieve values.
+Just as we saw with *args, the name of kwargs is completely arbitrary.
+"""
+tables = {
+  1: {
+    'name': 'Chioma',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes'
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+print(tables)
+
+
+# Write your code below: 
+def assign_food_items(**order_items):
+  print(order_items)
+  food = order_items.get('food')
+  drinks = order_items.get('drinks')
+  print(food)
+  print(drinks)
+
+# Example Call
+assign_food_items(food='Pancakes, Poached Egg', drinks='Water')
