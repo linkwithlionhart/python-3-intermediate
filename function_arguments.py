ad959fad5ae473bccdfe2cf267c8dd2e2dc70944
linkwithlionhart <linkwithlionhart@gmail.com>
2024-10-23 20:35:58 -0700

    more evening practice

--- function_arguments.py
+++ function_arguments.py
@@ -135,3 +135,58 @@ def assign_food_items(**order_items):
 
 # Example Call
 assign_food_items(food='Pancakes, Poached Egg', drinks='Water')
+
+tables = {
+  1: {
+    'name': 'Chioma',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes'
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+
+def assign_table(table_number, name, vip_status=False): 
+  tables[table_number]['name'] = name
+  tables[table_number]['vip_status'] = vip_status
+  tables[table_number]['order'] = {}
+
+assign_table(2, 'Douglas', True)
+print('--- tables with Douglas --- \n', tables)
+
+def assign_food_items(table_number, **order_items):
+  food = order_items.get('food')
+  drinks = order_items.get('drinks')
+  tables[table_number]['order']['food_items'] = food
+  tables[table_number]['order']['drinks'] = drinks
+
+assign_food_items(2, food='Seabass, Gnocchi, Pizza', drinks='Margarita, Water')
+print('\n --- tables after update --- \n', tables)
+
+#5 All together now! *args and *kwargs
+"""
+So far we have seen how both *args and **kwargs can be combined with standard arguments. 
+This is useful, but in some cases, we may want to use all three types together! 
+Thankfully Python allows us to do so as long as we follow the correct order in our function definition. 
+The order is as follows:
+
+1. Standard positional arguments
+2. *args
+3. Standard keyword arguments
+4. **kwargs
+"""
+# Write your code below: 
+def single_prix_fixe_order(appetizer, *entrees, sides, **dessert_scoops):
+  print(appetizer)
+  print(entrees)
+  print(sides)
+  print(dessert_scoops)
+
+single_prix_fixe_order('Baby Beets', 'Salmon', 'Scallops', sides='Mashed Potatoes', scoop_1='Vanilla', scoop_2 = 'Cookies and Cream')
