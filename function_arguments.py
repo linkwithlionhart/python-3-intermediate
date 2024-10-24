decb8ceef27f1f2a96c1eb482d4df731d36a5992
linkwithlionhart <linkwithlionhart@gmail.com>
2024-10-24 08:19:13 -0700

    more morning practice

--- function_arguments.py
+++ function_arguments.py
@@ -234,3 +234,49 @@ How to combine all different types of arguments to gain the most flexibility in 
 How to use an unpacking operator (* or **) to unpack arguments in a function call.
 How to use an unpacking operator (* or **) on iterables.
 """
+tables = {
+  1: {
+    'name': 'Jiho',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes',
+      'total': [534.50, 20.0, 5]
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+
+def assign_table(table_number, name, vip_status=False): 
+  tables[table_number]['name'] = name
+  tables[table_number]['vip_status'] = vip_status
+  tables[table_number]['order'] = {}
+
+def assign_food_items(table_number, **order_items):
+  food = order_items.get('food')
+  drinks = order_items.get('drinks')
+  tables[table_number]['order']['food_items'] = food
+  tables[table_number]['order']['drinks'] = drinks
+
+def calculate_price_per_person(total, tip, split):
+    total_tip = total * (tip/100)
+    split_price = (total + total_tip) / split
+    print(split_price)
+
+# The ability to remove a table’s guests when they leave the restaurant.
+leave_info = [1, True]
+def remove_guest(table_number, leave=True):
+  if leave == True:
+    tables[table_number] = {}
+    print(f'Table {table_number} is cleared.')
+  else:
+    print(f'Error. Guest has not left.')
+remove_guest(*leave_info)
+print(tables)
+
+# An adjustment to the calculate_price_per_person() function to access a tables 'total' and return the result.
